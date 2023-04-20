b90db29c95624e141a7073be537b788aac26fd08
Patricio Cordoba <patricio.cordoba@parserx.com>
2023-04-20 19:15:18 -0300

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
 import './App.css';
 
 function App() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyBwnGHauDu6fcE0AIO2f3wdMyPxdoeK--I',
+    });
+    const center = useMemo(() => ({ lat: -34.58638551527179, lng: -58.40026132075488 }), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <h1>Respirar - Modulo Mapas</h1>
+        {!isLoaded ? (
+            <h1>Loading...</h1>
+        ) : (
+            <GoogleMap
+                mapContainerClassName="map-container"
+                center={center}
+                zoom={10}
+            >
+                <Marker position={{ lat: -34.58638551527179, lng: -58.40026132075488 }}  icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}/>
+            </GoogleMap>
+        )}
     </div>
   );
 }
