3240efe368199a7546d456a63a2eb1c3e29a02fc
Patricio Cordoba <patricio.cordoba@parserx.com>
2023-04-24 22:26:18 -0300

    Added mocked data and refactor

--- src/App.js
+++ src/App.js
@@ -2,71 +2,21 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Map } from "./components/map/map";
 import { Navbar } from "./components/navbar/navbar";
+import { getStations } from "./services/stationsSerivice";
 
 function App() {
-  const [coords, setCoords] = useState({
-    latitude: "-34.58638551527179",
-    longitude: "-58.40026132075488",
-  });
-  const [cityName, setCityName] = useState("");
-
-  const options = {
-    enableHighAccuracy: true,
-    maximumAge: 30000,
-    timeout: 27000,
-  };
+  const [stations, setStations] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      getCurrentCityName,
-      error,
-      options
-    );
-  }, []);
-
-  const error = (err) => {
-    if (
-      err.code === 1 || //if user denied accessing the location
-      err.code === 2 || //for any internal errors
-      err.code === 3 //error due to timeout
-    ) {
-      alert(err.message);
-    } else {
-      alert(err);
-    }
-  };
-
-  const getCurrentCityName = (position) => {
-    setCoords({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
+    getStations(undefined).then((mappedStations) => {
+      setStations(mappedStations);
     });
-
-    let url =
-      "https://nominatim.openstreetmap.org/reverse?format=jsonv2" +
-      "&lat=" +
-      coords.latitude +
-      "&lon=" +
-      coords.longitude;
-
-    fetch(url, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Access-Control-Allow-Origin": "https://o2cj2q.csb.app",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setCityName(data.display_name));
-  };
+  }, []);
 
   return (
     <div>
-      {/* START NAV BAR */}
       <Navbar />
-      {/* END NAV BAR */}
-
-      <Map coords={coords} dispaly_name={cityName} />
+      <Map stations={stations} />
     </div>
   );
 }
--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,11 +1,48 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import icon from "../../images/pinIcon.png";
 import L from "leaflet";
 
-export const Map = ({ coords, display_name }) => {
-  const { latitude, longitude } = coords;
+export const Map = ({ stations }) => {
+  // default on Buenos Aires (if user does not accept location access request
+  const [coords, setCoords] = useState({
+    latitude: "-34.58638551527179",
+    longitude: "-58.40026132075488",
+  });
+
+  const options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000,
+  };
+
+  const error = (err) => {
+    if (
+      err.code === 1 || //if user denied accessing the location
+      err.code === 2 || //for any internal errors
+      err.code === 3 //error due to timeout
+    ) {
+      alert(err.message);
+    } else {
+      alert(err);
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      centerMapOnUserLocation,
+      error,
+      options
+    );
+  }, []);
+
+  const centerMapOnUserLocation = (position) => {
+    setCoords({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  };
 
   const customIcon = new L.Icon({
     //creating a custom icon to use in Marker
@@ -16,7 +53,7 @@ export const Map = ({ coords, display_name }) => {
 
   const MapView = () => {
     let map = useMap();
-    map.setView([latitude, longitude], map.getZoom());
+    map.setView([coords.latitude, coords.longitude], map.getZoom());
     //Sets geographical center and zoom for the view of the map
     return null;
   };
@@ -24,7 +61,7 @@ export const Map = ({ coords, display_name }) => {
   return (
     <MapContainer
       classsName="map"
-      center={[latitude, longitude]}
+      center={[coords.latitude, coords.longitude]}
       zoom={10}
       scrollWheelZoom={true}
     >
@@ -33,9 +70,32 @@ export const Map = ({ coords, display_name }) => {
         contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker icon={customIcon} position={[latitude, longitude]}>
-        <Popup>{display_name}</Popup>
-      </Marker>
+      {stations.map((station) => {
+        console.log(station);
+        return (
+          // TODO hacer bien este markup que es horrible
+          <Marker
+            icon={customIcon}
+            position={[
+              station.coordinates.latitude,
+              station.coordinates.longitude,
+            ]}
+          >
+            <Popup>
+              <span>Estación: {station.name}</span> <br />
+              <span>Temperatura: {station.temperature}ºC</span>
+              <br />
+              <span>Calidad pm1: {station.pm1}</span>
+              <br />
+              <span>Calidad pm10: {station.pm10}</span>
+              <br />
+              <span>Calidad pm25: {station.pm25}</span>
+              <br />
+            </Popup>
+          </Marker>
+        );
+      })}
+
       <MapView />
     </MapContainer>
   );
