fe7fe434c813f9da6a6461f93c8cbc4d21629552
Patricio Cordoba <patricio.cordoba@parserx.com>
2023-05-25 23:25:50 -0300

    feat: added some style

--- src/components/search/SearchAndResults.jsx
+++ src/components/search/SearchAndResults.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from "react";
 import Form from "react-bootstrap/Form";
-import {Button} from "react-bootstrap";
+import {Button, Image} from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import icon from "../../images/pinIcon.png";
 
 export const SearchAndResults = ({stations, setCoords}) => {
     const [searchWords, setSearchWords] = useState("");
@@ -21,16 +22,23 @@ export const SearchAndResults = ({stations, setCoords}) => {
         setCoords(coordinates)
     }
 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && event.shiftKey === false) {
+            event.preventDefault();
+            handleSearch();
+        }
+    };
+
     return (
         <div className="">
             <Form>
                 <Row className="mb-2">
                 <Form.Group as={Col} className="mb-3 me-2" controlId="search">
-                    <Form.Control onChange={e => setSearchWords(e.target.value )}  type="text" placeholder="Buscá tu estación" />
+                    <Form.Control onKeyDown={e => { handleKeyDown(e)}} onChange={e => setSearchWords(e.target.value )}  type="text" placeholder="Buscá tu estación" />
                     <Form.Text  />
                 </Form.Group>
                     <Form.Group as={Col} className="mb-3" controlId="search">
-                <Button onClick={handleSearch} variant="primary" >
+                <Button onClick={handleSearch} variant="primary" type="button" >
                     Buscar
                 </Button>
                     </Form.Group>
@@ -38,9 +46,14 @@ export const SearchAndResults = ({stations, setCoords}) => {
             </Form>
 
             <div>
+                {foundStations.length &&
+                    <Row className="resultados" >
+                        <span>Resultados:</span>
+                    </Row>
+                }
                 {foundStations && foundStations.map(station => {
                     return (      <div>
-                        <Button className="boton"  onClick={() => handleResultClick(station.coordinates)} > {station.name} </Button>
+                         <Button className="boton"  onClick={() => handleResultClick(station.coordinates)} > <Image src={icon}  className="pinResultado"/> {station.name} </Button>
                     </div>)
 
 
